2aff0fe02e2d5f4081b1b69afa176a0c23a09c08
vinthund <fleetfootgreyhound@gmail.com>
2021-10-12 17:21:20 +0100

    <feat> now returns CPU usage in percentage

--- systemStats.sh
+++ systemStats.sh
@@ -1,4 +1,4 @@
-2#author: vinthund
+#author: vinthund
 #released: 05102021
 #version: 0.0.1
 AUTHOR="vinthund"
@@ -26,17 +26,20 @@ fi
 #frequently scripts are written so that arguments can be passed in any order us$
 #with the flags method some arguments can be made mandatory or optional 
 #example: a:b (a is mandatory, b is optional) abc is all optional 
-while getopts tiv OPTION 
+while getopts ctiv OPTION 
 do 
 case ${OPTION}
 in 
-i) ipconfig wlan0 |  grep -w inet |  awk '{print$2}')
-	echo ${IP}
+i) ipconfig wlan0 |  grep -w inet |  awk '{print$2}'
+	echo ${IP};;
 v) echo -e "systemStats:\n\t Version: ${VERSION} Released: ${RELEASED} Author: ${AUTHOR}";;
 t) TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
    echo ${TEMP} "need to divide by thousand";;
+c) USAGE=$(grep -w 'cpu' /proc/stat | awk '{usage=($2+$3+$4+$6+$7+$8)*100/($2+$3+$4+$5+$6+$7+$8)}
+					   {free=($5)*100/($2+$3+$4+$5+$6+$7+$8)}
+					   END  {printf "used CPU: %.2f%%\n",usage}
+					        {printf "Free CPU: %.2f%%\n",free}')
+   echo ${USAGE};;
 esac
 done
-
 #end of script
-
